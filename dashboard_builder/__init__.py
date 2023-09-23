8c4e7bf6850a8dcf1b5533e20bad0072cac6cfbc
Hants Williams <hantsawilliams@gmail.com>
2023-09-23 12:27:44 -0400

    trouble shooting

--- dashboard_builder/__init__.py
+++ dashboard_builder/__init__.py
@@ -7,5 +7,13 @@
 
 import importlib.resources as pkg_resources
 
+def list_all_resources_in_package():
+    return pkg_resources.contents('dashboard_builder.templates')
+
+print(list_all_resources_in_package())
+
+content = pkg_resources.read_text('dashboard_builder.templates', 'base.html')
+print(content)
+
 def get_template(template_name):
     return pkg_resources.read_text('dashboard_builder.templates', template_name)
