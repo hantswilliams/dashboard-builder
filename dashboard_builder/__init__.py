ccad819cb480c4646380b87d657ea000c5e51177
Hants Williams <hantsawilliams@gmail.com>
2023-09-23 12:32:30 -0400

    trying anoterh way

--- dashboard_builder/__init__.py
+++ dashboard_builder/__init__.py
@@ -1,8 +1,8 @@
-import importlib.resources as pkg_resources
+import os
 
 def get_template(template_name):
-    try:
-        content = pkg_resources.read_text('dashboard_builder.templates', template_name)
-        return content
-    except FileNotFoundError:
-        return f"Template {template_name} not found!"
+    current_dir = os.path.dirname(__file__)  # This gets the directory where __init__.py resides
+    template_path = os.path.join(current_dir, 'templates', template_name)
+    
+    with open(template_path, 'r') as file:
+        return file.read()
