a3118b83953237c010ecc9b544ef062e3ef00cb8
Hants Williams <hantsawilliams@gmail.com>
2023-09-23 21:16:12 -0400

    added vercel deployment of example app

--- example_dashboards/app1/app.py
+++ example_dashboards/app1/app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template_string, request
 import pandas as pd
 
-#### For local dev testing....//otherwise turn off - comment out below ###
-import os 
-import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
-##########################################################################
+# #### For local dev testing....//otherwise turn off - comment out below ###
+# import os 
+# import sys
+# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+# ##########################################################################
 
 from dashboard_builder import get_dashboard_template # noqa: E402
 from dashboard_builder.config import Config # noqa: E402
