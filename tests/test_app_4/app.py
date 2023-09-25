984eeeb3f60bc7bd0cb394d2a956b930cae86144
Hants Williams <hantsawilliams@gmail.com>
2023-09-24 21:55:58 -0400

    added in the ability to add custom templates by user

--- tests/test_app_4/app.py
+++ tests/test_app_4/app.py
@@ -7,7 +7,7 @@ import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 ##########################################################################
 
-from dashboard_builder import get_dashboard_template  # noqa: E402
+from dashboard_builder import get_dashboard_template, get_dashboard_template_custom  # noqa: E402, E501, F401
 from dashboard_builder.config import Config # noqa: E402
 from dashboard_builder.components.outputs import OutputChart_Matplotlib # noqa: E501, E402
 from dashboard_builder.components.managers import ComponentManager # noqa: E402, E501
@@ -15,6 +15,7 @@ from dashboard_builder.components.managers import ComponentManager # noqa: E402,
 app = Flask(__name__)
 
 dashboard_settings = Config(
+    custom_template_dir="custom_templates_app4",
     footer_text="Built by Hants Williams, PhD, RN - Clinical Assistant Professor - Stony Brook University, School of Health Professions - Applied Health Informatics" # noqa: E501
     )
 
@@ -28,11 +29,11 @@ def index():
     ax = fig.add_subplot()
     ax.plot([1, 2, 3])
 
-    output12 = OutputChart_Matplotlib(fig)
-    manager.register_outputs(output12)
+    manager.register_outputs(OutputChart_Matplotlib(fig))
 
     return render_template_string(
-        get_dashboard_template('base'),
+        # get_dashboard_template('base'),
+        get_dashboard_template_custom('my_custom_template.j2', dashboard_settings),
         output_components=manager.render_outputs(),
         settings=dashboard_settings
     )
